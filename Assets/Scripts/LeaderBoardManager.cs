115f3b1523de7ebb6a714f53f619b3f3a765cd18
NNADuong-CS <duong342005@gmail.com>
2025-03-27 17:17:38 +0700

    UpdateNew

---
+++ Assets/Scripts/LeaderBoardManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderBoardManager : MonoBehaviour
+{
+    public GameObject panel; // Panel cần mở/đóng
+    public Button  openButton; // Nút mở panel
+    public Button returnButton; // Nút đóng panel
+
+    public void OpenPanel()
+    {
+        panel.SetActive(true);
+    }
+    public void ClosePanel()
+    {
+        panel.SetActive(false);
+    }
+}
