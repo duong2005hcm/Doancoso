f5fca04d5faa254461381d6d57f3c06f0e1ef59b
NNADuong-CS <duong342005@gmail.com>
2025-03-27 15:48:46 +0700

    UpdateBXH

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,15 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void StartButtonClicked()
+    [Header("UI Buttons")]
+    public Button StartGameButton; // Nút bắt đầu trò chơi
+    public Button ShopButton; // Nút vào cửa hàng (nếu có)
+
+    private void Start()
     {
-        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
-    }
-    public void ShopButtonClicked()
-    {
-        SceneManager.LoadScene("Shop", LoadSceneMode.Single);
+        // Gán sự kiện khi bấm nút
+        StartGameButton.onClick.AddListener(StartButtonClicked);
+
+        if (ShopButton != null) // Kiểm tra nếu có nút Shop
+        {
+            ShopButton.onClick.AddListener(ShopButtonClicked);
+        }
     }
 
+    private void StartButtonClicked()
+    {
+        Debug.Log("Bắt đầu game!");
+        SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+
+    private void ShopButtonClicked()
+    {
+        Debug.Log("Vào Shop!");
+        SceneManager.LoadScene("Shop", LoadSceneMode.Single);
+    }
 }
